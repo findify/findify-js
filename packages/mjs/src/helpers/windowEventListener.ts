4a69a0126b61f0af2d3b9cdeba7bce6270dd5c37
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2017-09-28 20:14:25 +0300

    WIP(mjs): add a fix from old mjs repo
    affects: @findify/mjs

--- packages/mjs/src/helpers/windowEventListener.ts
+++ packages/mjs/src/helpers/windowEventListener.ts
@@ -1,4 +1,4 @@
-const listeners = (node: any = window) => ({
+const listeners = (node: any = document) => ({
   on: (events, cb) => events.forEach(evt => node.addEventListener(evt, cb)),
   off: (events, cb) => events.forEach(evt => node.removeEventListener(evt, cb)),
 });
