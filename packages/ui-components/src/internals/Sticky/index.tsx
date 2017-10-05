fdbc0d78e543e5e3101744a04b9c862c7ae24bd5
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2017-10-05 12:30:45 +0300

    feat(ui-components): simplify sticky filters implementation
    affects: @findify/ui-components

--- packages/ui-components/src/internals/Sticky/index.tsx
+++ packages/ui-components/src/internals/Sticky/index.tsx
@@ -21,16 +21,12 @@ export type OwnProps = {
 export type Handlers = {
   setRef(element: HTMLDivElement): void;
   setSticky(on: boolean): void;
-  onScroll(event: Event): void;
 };
 
 export type State = {
   isSticky?: boolean;
   target?: HTMLDivElement;
   style?: React.CSSProperties;
-  stickOnTop: boolean;
-  stickOnBottom: boolean;
-  scrollTop: number;
 };
 
 export type Props = OwnProps &
@@ -44,10 +40,6 @@ export const Sticky = compose<OwnProps, Props>(
   withState('state', 'setState', () => ({
     target: null,
     isSticky: false,
-    stickOnTop: false,
-    stickOnBottom: false,
-    style: {},
-    scrollTop: window.pageYOffset || document.documentElement.scrollTop,
   })),
   withHandlers({
     setSticky: ({ state }: Props) => (on: boolean) => {
@@ -63,45 +55,18 @@ export const Sticky = compose<OwnProps, Props>(
         setState({ ...state, target });
       }
     },
-    onScroll: ({ state, setState }: Props) =>
-      throttle((event: Event) => {
-        if (!state.target) return;
-        const rect = state.target.children[0].getBoundingClientRect();
-        const windowHeight =
-          window.innerHeight || document.documentElement.clientHeight;
-        const scrollTop =
-          window.pageYOffset || document.documentElement.scrollTop;
-        const delta = scrollTop - state.scrollTop;
-        const isScrollingDown = delta > 0;
-
-        const stickOnBottom = rect.bottom < windowHeight;
-        const stickOnTop = !isScrollingDown && rect.top >= 0;
-
-        let style = {};
-        if (stickOnBottom) {
-          style = { bottom: 0 };
-        } else if (stickOnTop) {
-          style = { top: 0 };
-        }
-        setState({ ...state, scrollTop, style, stickOnTop, stickOnBottom });
-      }, 200),
   }),
   lifecycle<Props, State>({
     componentDidMount() {
-      window.addEventListener('scroll', this.props.onScroll, false);
       this.props.setSticky(true);
     },
     componentWillMount() {
       this.props.setSticky(false);
-      window.removeEventListener('scroll', this.props.onScroll, false);
     },
   })
 )(({ className, style, state, children, setRef }: Props) => (
   <div
-    className={cx(styles.sticky, className, {
-      [styles.top]: state.stickOnTop,
-      [styles.bottom]: state.stickOnBottom,
-    })}
+    className={cx(styles.sticky, className)}
     style={{ ...style, ...state.style }}
     ref={setRef}
   >
