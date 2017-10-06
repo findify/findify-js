23608de4325a65c5b1fc7e21c04860f34a23e751
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2017-10-06 15:39:26 +0300

    chore(*): remove example pkgs to speed up build on travis ci

--- packages/quux/src/__tests__/index.test.ts
+++
@@ -1,7 +0,0 @@
-import { zero, quux, woof } from '..';
-
-test('my passing test', () => {
-  expect(zero()).toBe(0);
-  expect(quux(2)).toBe(2);
-  expect(woof()).toBe(-1);
-});
--- packages/quuz/src/index.ts
+++
@@ -1,6 +0,0 @@
-import eight from '@findify/qux';
-import { qux } from '@findify/qux';
-import { quux } from '@findify/quux';
-
-export const quuz1 = x => qux(x) * 2 + quux(x + 1) + eight() - 2;
-export const quuz2 = () => 'quuz2';
