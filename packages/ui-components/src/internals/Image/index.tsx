86598070bc92a636dd7d3747fd75f159f8e2b630
Bitaru <i@bitaru.com>
2017-10-18 09:53:25 +0300

    WIP(ui-components): implement basic image logic
    affects: @findify/ui-components

--- packages/ui-components/src/internals/Image/index.tsx
+++ packages/ui-components/src/internals/Image/index.tsx
@@ -6,7 +6,8 @@ import {
   pure,
   withProps,
   mapProps,
-  withState,
+  withStateHandlers,
+  withPropsOnChange,
 } from 'recompose';
 import * as cx from 'classnames';
 import { defer } from 'lodash';
@@ -20,39 +21,80 @@ export interface OwnProps {
   onClick?: React.MouseEventHandler<HTMLImageElement>;
 }
 
+/**
+ * STAGE:
+ * 0 - nothing loaded
+ * 1 - thumbnail loaded
+ * 2 - image loaded
+*/
 export interface State {
-  isMounted: boolean;
-  isLoading: boolean;
-  setIsMounted: (isMounted: boolean) => void;
-  setIsLoading: (isLoading: boolean) => void;
+  src: string;
+  stage: bumber;
+  setSrc: (isMounted: boolean) => void;
+  setThumbnail: (isLoading: boolean) => void;
 }
 
 export type MappedProps = OwnProps & { src?: string };
 export type Props = MappedProps & State;
 
+const fetch = (src: string) =>
+  new Promise(resolve => {
+    const img = new Image();
+    img.addEventListener('load', () => resolve(src), false);
+    img.src = src;
+  });
+
+const getShopifyUrl = (
+  src: string,
+  isThumb: boolean,
+  width?: number,
+  height?: number
+) => {
+  const size = isThumb ? '10x' : !!width ? `${width}x` : 'medium';
+  return src.replace(/_(medium|large)./g, `_${size}.`);
+};
+
 const ImageComponent = compose<OwnProps, Props>(
   pure,
-  withState('isLoading', 'setIsLoading', true),
-  withState('isMounted', 'setIsMounted', true),
-  lifecycle<OwnProps & State, OwnProps & State>({
-    componentWillMount() {
-      const img = new Image();
-      img.onload = (ev: Event) =>
-        this.props.isMounted && this.props.setIsLoading(false);
-      img.src = this.props.src;
-    },
-    componentWillUnmount() {
-      this.props.setIsMounted(false);
-    },
+  withPropsOnChange(['src'], ({ src, width, height }) => {
+    if (src.includes('.shopify')) {
+      return {
+        src: void 0,
+        original: getShopifyUrl(src, false, width, height),
+        thumbnail: getShopifyUrl(src, true),
+      };
+    }
+    return { src: void 0, original: src };
   }),
-  mapProps(
-    ({ isLoading, className, src, alt, setIsLoading, onClick }: Props) => ({
-      alt,
-      onClick,
-      src: isLoading ? undefined : src,
-      className: cx(className, (isLoading && styles.loading) || styles.loaded),
-    })
+  withStateHandlers(
+    { src: void 0, stage: 0 },
+    {
+      setSrc: () => src => ({ src, stage: 2 }),
+      setThumbnail: () => src => ({ src, stage: 1 }),
+    }
   ),
+  withPropsOnChange(['^'], ({ setSrc, setThumbnail, thumbnail, original }) => {
+    if (thumbnail) {
+      fetch(thumbnail)
+        .then(setThumbnail)
+        .then(() => fetch(original))
+        .then(setSrc);
+    } else {
+      fetch(original).then(setSrc);
+    }
+  }),
+  mapProps(({ stage, className, src, alt, onClick }: Props) => ({
+    alt,
+    onClick,
+    src,
+    className: cx(
+      className,
+      styles.image,
+      stage === 0 && styles.loading,
+      stage === 1 && styles.thumbnail,
+      stage === 2 && styles.original
+    ),
+  })),
   onlyUpdateForKeys(['src'])
 )(props => <img {...props} />);
 
