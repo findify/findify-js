d3b25243fdab9e48b9e914fe293758c49de652e1
Bitaru <i@bitaru.com>
2018-01-25 16:00:34 +0300

    chore(sdk): update types
    affects: @findify/helpers, @findify/sdk, @findify/ui-components

--- packages/sdk/src/request/SmartCollection.ts
+++ packages/sdk/src/request/SmartCollection.ts
@@ -2,17 +2,18 @@ import { Type as RequestType } from './type';
 import { List } from './params';
 
 /**
- * Smart collection request.
- */
-export interface Request {
-  type: RequestType.SmartCollection;
-  params: Params;
-}
-
-/**
  * Smart collection request parameters.
  */
 export interface Params extends List {
   /** Collection handle */
   slot: string;
+}
+
+
+/**
+ * Smart collection request.
+ */
+export interface Request {
+  type: RequestType.SmartCollection;
+  params: Params;
 }
