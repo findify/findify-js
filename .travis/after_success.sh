0f5cb5cb7fdfa79f357d4a8edce1d110f76fbe60
Bitaru <i@bitaru.com>
2018-01-26 16:20:36 +0300

    chore(agent, sdk): deploy pkgs to s3
    affects: @findify/agent, @findify/sdk

--- .travis/after_success.sh
+++ .travis/after_success.sh
@@ -73,7 +73,7 @@ if [[ $TRAVIS_BRANCH == 'master' ]]; then
 
 fi
 
-PKGS=(analytics helpers mjs)
+PKGS=(analytics helpers mjs sdk agent)
 export GIT_SHA=$(git rev-parse HEAD)
 
 ASSET_HOST="https://findify-assets-2bveeb6u8ag.netdna-ssl.com"
