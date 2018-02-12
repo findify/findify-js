82e90c88108502b87374dbf79eb225559d0af85a
Bitaru <i@bitaru.com>
2018-02-12 17:55:25 +0300

    fix(react-connect): bump version and remove reexports
    affects: @findify/react-connect

--- packages/react-connect/src/index.ts
+++ packages/react-connect/src/index.ts
@@ -1,13 +1,10 @@
-export * from './provider';
-
-export { default as createConnect } from './connect/createConnect';
-
 export { createProvider } from './provider/createProvider';
 export { Autocomplete as AutocompleteProvider } from './provider/index';
 export { Search as SearchProvider } from './provider/index';
 export { Recommendation as RecommendationProvider } from './provider/index';
 export { SmartCollection as SmartCollectionProvider } from './provider/index';
 
+export { default as createConnect } from './connect/createConnect';
 export { default as connectSuggestions } from './connect/connectSuggestions';
 export { default as connectItems } from './connect/connectItems';
 export { default as connectBreadcrumbs } from './connect/connectBreadcrumbs';
