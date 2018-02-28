3544cb7aaf63973fd46b2a62e26f1e13e7021f38
Andrey Sinitsyn <andrey.sin98@gmail.com>
2018-02-28 08:24:02 +0300

    WIP(react-components): add base of ItemCard component
    affects: @findify/react-components

---
+++ packages/react-components/src/components/ItemCard/view.tsx
@@ -0,0 +1,10 @@
+import react from 'react'
+
+const ItemCard = ({ item }) => (
+  <div className="findify__item-card">
+    <h4 className="findify__item-card__title">{item.get('title')}</h4>
+    <img className="findify__item-card__image" src={item.get('image_url')} />
+  </div>
+)
+
+export default ItemCard
