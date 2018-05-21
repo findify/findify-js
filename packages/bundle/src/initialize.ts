a0cc70e5bc27f56225b035df609d9e005db6bb85
Andrey Sinitsyn <andrey.sin98@gmail.com>
2018-05-21 10:20:28 +0300

    WIP(bundle,react-connect): fix incorrect analytics key in config
    affects: @findify/bundle, @findify/react-connect

--- packages/bundle/src/initialize.ts
+++ packages/bundle/src/initialize.ts
@@ -52,7 +52,7 @@ export default async (
 
   __root.config = fromJS(cfg);
 
-  __root.analytics = Analytics({ ...cfg.platform, key: cfg.key, events: cfg.events });
+  __root.analytics = Analytics({ ...cfg.platform, key: cfg.key, events: cfg.analytics || {} });
 
   await documentReady;
 
