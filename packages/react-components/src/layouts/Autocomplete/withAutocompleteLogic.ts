7ec90295d58cc615763eed0e548f4922c53f4468
Andrey Sinitsyn <andrey.sin98@gmail.com>
2018-06-06 15:46:27 +0300

    WIP(bundle,react-components): fix arrow navigation not submitting suggestion on Enter
    affects: @findify/bundle, @findify/react-components

--- packages/react-components/src/layouts/Autocomplete/withAutocompleteLogic.ts
+++ packages/react-components/src/layouts/Autocomplete/withAutocompleteLogic.ts
@@ -6,35 +6,40 @@ export default compose(
   setDisplayName('withAutocompleteLogic'),
   connectSuggestions,
   withStateHandlers({ selectedSuggestion: -1, }, {
-    changeSuggestionIndex: ({ selectedSuggestion }, { config, suggestions, getSuggestionProps }) => (evt) => {
-      const arrowCodes = ['ArrowUp', 'ArrowDown']
-      if (evt.key === 'Enter' && selectedSuggestion !== -1) {
-        evt.preventDefault()
-        getSuggestionProps(selectedSuggestion, config.get('widgetKey')).onClick()
-        return;
+    changeSuggestionIndex: ({ selectedSuggestion }, { config, suggestions, getSuggestionProps }) => {
+      const node: HTMLInputElement = config.get('node')!;
+      return (evt) => {
+        const arrowCodes = ['ArrowUp', 'ArrowDown']
+        if (evt.target !== node) return;
+        if (evt.key === 'Enter' && selectedSuggestion !== -1) {
+          evt.stopPropagation();
+          evt.preventDefault();
+          getSuggestionProps(selectedSuggestion, config.get('widgetKey')).onClick();
+          return;
+        }
+        if (!arrowCodes.includes(evt.key)) return;
+        evt.preventDefault();
+
+        const newSuggestionIndex = selectedSuggestion + (evt.key === 'ArrowUp' ? -1 : 1)
+        const totalSuggestions = suggestions && suggestions.size || 0
+
+
+        if (newSuggestionIndex < 0) return { selectedSuggestion: totalSuggestions - 1 }
+        if (totalSuggestions - 1 < newSuggestionIndex) return  { selectedSuggestion: 0 }
+        return { selectedSuggestion: newSuggestionIndex }
       }
-      if (!arrowCodes.includes(evt.key)) return;
-      evt.preventDefault();
-
-      const newSuggestionIndex = selectedSuggestion + (evt.key === 'ArrowUp' ? -1 : 1)
-      const totalSuggestions = suggestions && suggestions.size || 0
-
-
-      if (newSuggestionIndex < 0) return { selectedSuggestion: totalSuggestions - 1 }
-      if (totalSuggestions - 1 < newSuggestionIndex) return  { selectedSuggestion: 0 }
-      return { selectedSuggestion: newSuggestionIndex }
     },
     setSuggestionIndex: ({ selectedSuggestion }) => (index) => ({ selectedSuggestion: index })
   }),
   lifecycle({
     componentDidMount() {
       const node: HTMLInputElement = this.props.config.get('node');
-      node.addEventListener('keydown', this.props.changeSuggestionIndex)
+      document.querySelector('body')!.addEventListener('keydown', this.props.changeSuggestionIndex, true)
       node.autocomplete = 'off';
     },
     componentWillUnmount() {
       const node: HTMLInputElement = this.props.config.get('node')
-      node.removeEventListener('keydown', this.props.changeSuggestionIndex)
+      document.querySelector('body')!.removeEventListener('keydown', this.props.changeSuggestionIndex, true)
     },
     componentWillReceiveProps(nextProps) {
       if (nextProps.meta.get('q') !== this.props.meta.get('q')) {
