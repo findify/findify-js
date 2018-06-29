800001c7906f2a3116b426d554dfb5b35b536fe5
Andrey Sinitsyn <andrey.sin98@gmail.com>
2018-06-29 13:36:33 +0300

    WIP(react-components): fix facets displaying incorrect representation of range
    affects: @findify/react-components

--- packages/react-components/src/components/RatingFacet/content.tsx
+++ packages/react-components/src/components/RatingFacet/content.tsx
@@ -8,11 +8,11 @@ import Icon from 'components/Icon';
 export default ({ item, theme, config }) =>
 <>
   {
-    [...Array(item.get('from') + 1).keys()]
+    [...Array(item.get('from')).keys()]
     .map((_, i) => <Icon className={theme.star} name='Star' key={'fill-' + i} />)
   }
   {
-    [...Array(5 - item.get('from') - 1).keys()]
+    [...Array(5 - item.get('from')).keys()]
     .map((_, i) => <Icon className={cx(theme.star, theme.unfilled)} name='Star' key={'unfill-' + i} />)
   }
 </>
