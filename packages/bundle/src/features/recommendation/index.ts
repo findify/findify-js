44a74994eb9a18fe774ac14ac2b6012956872e61
timram <rjckec@gmail.com>
2019-12-26 14:06:52 +0300

    fix(bundle): remove uneccessary spaces
    affects: @findify/bundle

--- packages/bundle/src/features/recommendation/index.ts
+++ packages/bundle/src/features/recommendation/index.ts
@@ -12,7 +12,7 @@ const lazyRecommendation = lazy(() => import(
 export default (widget) => {
   const { node, agent, config } = widget;
   const props = { agent, config, apiKey: config.get('key') };
-  if (!config.get('disableAutoRequest'  )) {
+  if (!config.get('disableAutoRequest')) {
     agent.defaults(getPayload(config, node, __root.analytics.state));
   }
 
