b2658d60a5dc8de910c1da8e7ce291459cafc622
Bitaru <i@bitaru.com>
2020-01-21 13:37:54 +0300

    fix(bundle, react-components): update custom layout and trigger page scroll on slider slide
    affects: @findify/bundle, @findify/react-components

--- packages/react-components/src/layouts/Recommendation/Slider/Swiper.tsx
+++ packages/react-components/src/layouts/Recommendation/Slider/Swiper.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Swiper, Navigation, Pagination } from 'swiper/js/swiper.esm.js';
 import ReactIdSwiper from 'react-id-swiper/lib/ReactIdSwiper.custom';
 import Icon from 'components/Icon';
@@ -8,6 +8,10 @@ const defaultProps = {
   modules: [Navigation, Pagination],
   renderPrevButton: () => <button className="swiper-button-prev"><Icon name='ArrowLeftBig' /></button>,
   renderNextButton: () => <button className="swiper-button-next" ><Icon name='ArrowRightBig' /></button>,
+  getSwiper: (swiper) => swiper.on('slideChangeTransitionEnd', () => {
+    window.scrollTo(window.scrollX, window.scrollY - 1);
+    window.scrollTo(window.scrollX, window.scrollY + 1);
+  })
 }
 
 export default ({ children, ...props }) => {
