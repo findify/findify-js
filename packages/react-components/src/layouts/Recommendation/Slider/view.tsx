b33025c9092a76065ddd71d24a8104f0debb5492
Christin Mikolai <mail@christinmikolai.de>
2021-02-05 17:25:32 +0100

    fix(react-components): switch to use Icon component
    affects: @findify/react-components

--- packages/react-components/src/layouts/Recommendation/Slider/view.tsx
+++ packages/react-components/src/layouts/Recommendation/Slider/view.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Swiper from 'layouts/Recommendation/Slider/Swiper';
 import ProductCard from 'components/Cards/Product'
 import Text from 'components/Text';
+import Icon from 'components/Icon';
 
 export default ({ items, config, theme, sliderOptions }) =>
 <React.Fragment display-if={items && items.size > 0}>
@@ -11,7 +12,7 @@ export default ({ items, config, theme, sliderOptions }) =>
 
   <div className="findify-swiper-wrapper">
     <button aria-label="previous" className={`findify-swiper-button-prev ${config.get('slot')}-prev`}>
-      <svg width="11" height="19" className="findify-components--icon" xmlns="http://www.w3.org/2000/svg"><path d="M3.064 9.507L11 1.539 9.468 0 0 9.507 9.454 19l1.532-1.539z" fill="currentColor"></path></svg>
+      <Icon name='ArrowLeftBig' className={theme.arrow}/>
     </button>
     <Swiper {...sliderOptions}>
       {
@@ -25,7 +26,7 @@ export default ({ items, config, theme, sliderOptions }) =>
       }
     </Swiper>
     <button aria-label="next" className={`findify-swiper-button-next ${config.get('slot')}-next`}>
-      <svg width="11" height="19" className="findify-components--icon" xmlns="http://www.w3.org/2000/svg"><path d="M7.936 9.507L0 1.539 1.532 0 11 9.507 1.546 19 .014 17.461z" fill="currentColor"></path></svg>
+      <Icon name='ArrowRightBig' className={theme.arrow}/>
     </button>
   </div>
 </React.Fragment>
