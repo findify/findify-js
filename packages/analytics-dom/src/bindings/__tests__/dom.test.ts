04f50fa32be28329409fecefd8698ea7e115abc8
Andrej Naumovski <andrej@findify.io>
2021-03-01 16:53:58 +0100

    fix: Fix failing tests after TS migration

--- packages/analytics-dom/src/bindings/__tests__/dom.test.ts
+++ packages/analytics-dom/src/bindings/__tests__/dom.test.ts
@@ -1,6 +1,6 @@
 import { startDOMListeners } from '../dom';
 
-let state = [];
+let state: any[] = [];
 const listener = (...args) => { state = args; };
 
 afterAll(() => {
@@ -13,10 +13,13 @@ describe('DOM events', () => {
     <button id='suggestion' data-findify-track='click-item' data-findify-id='1' data-findify-rid='2' />
   `
   it('Should handle click on element with data-* attrs', done => {
-    startDOMListeners(listener, document)
-    document.getElementById('suggestion').click();
-    expect(state[0]).toBe('click-item');
-    expect(state[1]).toEqual({ id: '1', rid: '2' });
-    done();
+    startDOMListeners(listener, document);
+    const suggestion = document.getElementById('suggestion');
+    if (suggestion) {
+      suggestion.click();
+      expect(state[0]).toBe('click-item');
+      expect(state[1]).toEqual({ id: '1', rid: '2' });
+      done();
+    }
   })
 })
