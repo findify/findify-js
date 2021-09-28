76a13185c78586bbb3384dd60607b51bbb99535e
Bitaru <i@bitaru.com>
2021-09-28 18:00:48 +0300

    fix(@findify/bundle): update widget types

--- packages/bundle/src/features/recommendation/payload.ts
+++ packages/bundle/src/features/recommendation/payload.ts
@@ -40,11 +40,7 @@ export const getPayload = (
     payload.rules = rulesToApply;
   }
 
-  if (['bought', 'viewed', 'viewedBought', 'viewedViewed'].includes(type)) {
-    payload.item_ids = [item_id];
-  }
-
-  if (type === 'purchasedTogether') {
+  if (['viewedBought', 'viewedViewed', 'purchasedTogether'].includes(type)) {
     payload.item_ids = multipleIds ? item_ids : [item_id];
   }
 
