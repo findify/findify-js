c6d77a3f5c249a7053fd6a9d1954cf2edf0449e9
Pascal Achkar <pascal@Pascals-MacBook-Pro.local>
2021-11-23 15:41:38 +0200

    style(@findify/react-components): remove unused condition

--- packages/react-components/src/components/search/SearchResultsLayout/index.tsx
+++ packages/react-components/src/components/search/SearchResultsLayout/index.tsx
@@ -15,11 +15,11 @@ interface IResultsLayoutProps {
 const ResultsLayout = ({ condition, ...props }: IResultsLayoutProps) => {
   switch (condition) {
     case 'lazy':
-      return <LazyResults {...(condition && props)} />;
+      return <LazyResults {...props} />;
     case 'static':
-      return <StaticResults {...(condition && props)} />;
+      return <StaticResults {...props} />;
     case 'combined':
-      return <CombinedResults {...(condition && props)} />;
+      return <CombinedResults {...props} />;
     default:
       return <></>;
   }
