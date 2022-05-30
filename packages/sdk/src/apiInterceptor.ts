6529cc23d225d8540b2f186857f67fa996c412cb
Warren Wolff <ww.warren.wolff@gmail.com>
2022-05-30 11:56:41 +0100

    Added apiInterceptor for every apiRequest

---
+++ packages/sdk/src/apiInterceptor.ts
@@ -0,0 +1,35 @@
+import { Request } from './api';
+
+export const requestInterceptor = (request: Request) => {
+    return { ...request, ...checkRequest(request) };
+}
+
+const checkRequest = (request: Request) => {
+    let requestChanges: any = {};
+
+    if (request.body) {
+        requestChanges.body = { ...request.body, ...checkBody(request.body) };
+    }
+
+    return requestChanges;
+}
+
+const checkBody = (body) => {
+    let bodyChanges: any = {};
+
+    if (body?.search) {
+        bodyChanges.search = { ...body.search, ...checkSearchParam(body.search) }
+    }
+    bodyChanges.lallero = 'lallero';
+    return bodyChanges;
+}
+
+const checkSearchParam = (search: any) => {
+    let changes: any = {};
+
+    if (search.from) {
+        changes.from = search.from < 1 ? 0 : search.from;
+    }
+
+    return changes;
+}
