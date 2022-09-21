108e345fbf80596bc3af868129df4e09def129cc
Warren Wolff <ww.warren.wolff@gmail.com>
2022-09-21 14:51:43 +0100

    wrap useScrollBackToProduct hook within try and catch

--- packages/react-components/src/helpers/useScrollBackToProduct.ts
+++ packages/react-components/src/helpers/useScrollBackToProduct.ts
@@ -4,8 +4,12 @@ export default (container, item, shouldScroll) => {
   const hash = document?.location.hash.substring(1);
 
   useEffect(() => {
-    if (!shouldScroll || !container.current || item.get('id') !== hash) return;
-    window.findify.utils.updateHash('')
-    setTimeout(() => container.current.scrollIntoView(), 500);
+    try {
+      if (!shouldScroll || !container.current || item.get('id') !== hash) return;
+      window.findify.utils.updateHash('')
+      setTimeout(() => container.current.scrollIntoView(), 500);
+    } catch (err) {
+      console.log(err);
+    }
   }, [container]);
 };
