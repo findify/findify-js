c14a419aabf7cdd97d0f90876a79f297d2bf1d28
Warren Wolff <ww.warren.wolff@gmail.com>
2022-11-28 17:19:59 +0000

    Injection function fix trial 1.1

--- packages/bundle/src/utils/inject.ts
+++ packages/bundle/src/utils/inject.ts
@@ -5,7 +5,14 @@ export const injectComponents = async (components) => {
     console.log('[MJS]: inject updated components', components);
     const newComponents = Object.keys(components).reduce((acc: any[], k) => {
         const key = components[k].hash ? components[k].hash : k;
-        const component = components[k] && components[k].code ? (typeof components[k].code === 'string' ? new Function('return ' + components[k].code) : components[k].code) : components[k];
+        let component;
+        if (!components[k]) {
+            return acc;
+        } else if (typeof components[k] === 'string') {
+            component = new Function('return ' + components[k]);
+        } else if (typeof components[k] === 'object') {
+            component = typeof components[k].code === 'string' ? new Function('return ' + components[k].code) : components[k].code;
+        }
         acc[key] = component;
         return acc;
     }, [])
