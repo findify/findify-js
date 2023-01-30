aac79c4fa67b83d345d2e5771095269a0040f000
Warren Wolff <ww.warren.wolff@gmail.com>
2023-01-30 15:27:32 +0000

    Aligned the 2 code base

--- packages/react-connect/src/utils/addBasepath.ts
+++ packages/react-connect/src/utils/addBasepath.ts
@@ -2,9 +2,9 @@
 const fallbackComputeBasepath = (url) => {
     // Check if there is country-code
     const regex = new RegExp(/^\/[a-zA-Z]{2}-[a-zA-Z]{2}\//, 'gi'); // Match -> /en-ca/
-    const countryCode = window.location.pathname.split('/').find(p => regex.test(`/${p}/`));
-    if (countryCode) {
-        return `/${countryCode}` + url;
+    const countryPath = window.location.pathname.split('/').find(p => regex.test(`/${p}/`));
+    if (countryPath) {
+        return `/${countryPath}` + url;
     } else {
         return url;
     }
