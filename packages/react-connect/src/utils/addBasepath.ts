90edd1fe3ba627e96a773f35b82bb800ce242f8b
Warren Wolff <ww.warren.wolff@gmail.com>
2023-01-30 15:21:52 +0000

    Check as well // to make regex more accurate

--- packages/react-connect/src/utils/addBasepath.ts
+++ packages/react-connect/src/utils/addBasepath.ts
@@ -1,11 +1,21 @@
-// tslint:disable-next-line:variable-name
-export const addBasepath = (url: string) => {
+// If integration do not rely on the bundle eg. window.findify is undefined then.
+const fallbackComputeBasepath = (url) => {
     // Check if there is country-code
-    const regex = new RegExp(/^[a-zA-Z]{2}-[a-zA-Z]{2}/, 'gi');
-    const countryPath = window.location.pathname.split('/').find(p => regex.test(p));
-    if (countryPath) {
-        return `/${countryPath}` + url;
+    const regex = new RegExp(/^\/[a-zA-Z]{2}-[a-zA-Z]{2}\//, 'gi'); // Match -> /en-ca/
+    const countryCode = window.location.pathname.split('/').find(p => regex.test(`/${p}/`));
+    if (countryCode) {
+        return `/${countryCode}` + url;
     } else {
         return url;
+    }
+}
+
+// tslint:disable-next-line:variable-name
+export const addBasepath = (url: string) => {
+    const basepath = (window as any)?.findify?.utils.getBasepath();
+    if (basepath) {
+        return basepath + url;
+    } else {
+        return fallbackComputeBasepath(url);
     }
 }
