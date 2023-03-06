fbb390db75c45562778234ef0b5c0f1be3b196f4
Warren Wolff <ww.warren.wolff@gmail.com>
2023-03-06 17:53:34 +0100

    Swiper Autoplay imported and included in use to not make it disappear by bundler

--- packages/react-components/src/components/Swiper/raw.tsx
+++ packages/react-components/src/components/Swiper/raw.tsx
@@ -1,8 +1,8 @@
 import { Children } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import SwiperCore, { Navigation, Pagination } from 'swiper';
+import SwiperCore, { Navigation, Pagination, Autoplay } from 'swiper';
 
-SwiperCore.use([Navigation, Pagination]);
+SwiperCore.use([Navigation, Pagination, Autoplay]);
 
 export default ({ children, ...props }) => {
   return (
