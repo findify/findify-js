127767f9e5c37bf6ac1cc29eba8e0c3f88e01133
Warren Wolff <ww.warren.wolff@gmail.com>
2023-08-21 11:11:48 +0100

    collections might be undefined nullcheck

--- packages/analytics/src/utils/location.ts
+++ packages/analytics/src/utils/location.ts
@@ -2,7 +2,7 @@ export const isCollection = (url) => {
     if (!url) url = window.location.pathname;
     // @ts-ignore
     const collections = window.findify?.config?.toJS().collections;
-    return collections.some(collection => url.includes(collection));
+    return collections?.some(collection => url.includes(collection));
 }
 
 export const isSearch = (url) => {
